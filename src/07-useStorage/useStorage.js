9cf506a7429aecf05b66ffbe028e3e31d5308230
Jason Tumusiime <jasongeorgetumusiime@gmail.com>
2021-01-18 16:04:42 +0300

    Add some note on useNetwork hook

--- src/07-useStorage/useStorage.js
+++ src/07-useStorage/useStorage.js
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react"
 
 export const useStorage = (key, initialState) => {
+  // Hook does not consider the initial state
+  // localStorage blocks the main thread
   let [state, setState] = useState(initialState)
 
   useEffect(() => {
@@ -10,6 +12,7 @@ export const useStorage = (key, initialState) => {
 
   return [state, state => {
     setState(state)
+    // localStorage blocks the main thread
     localStorage.setItem(key ,JSON.stringify(state))
   }]
 }
